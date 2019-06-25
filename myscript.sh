d83426fbd5bcf5b7cb2d2660341da8c959de4796
Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
2019-06-25 11:15:24 -0700

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
 # This is a comment
-
+# Script description: This script will iterate over all spreadsheets under /DATA/Spreadsheets and look for words
 
 cd /home/jupyter/IntroToBash/DATA/Spreadsheets/
 
 echo "This script processes DATA files"
 
+for x in *.csv;do grep "John" "$x";done
+
+# Sample command to create and print variables
 myVariable=DATA_1.csv
 echo ${myVariable}
 
-for x in *.csv;do grep "John" "$x";done
-
+# Sample command to run a Python script
 python scriptname.py variables 
+
+
 
 
 
