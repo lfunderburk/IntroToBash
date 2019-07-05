624f10f51dcfc25c035ab9618a7dd3b1b53c732d
Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
2019-07-05 13:00:45 -0700

    add hello world sh

---
+++ Scripts/hello.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --time=00:10:00
+#SBATCH --mem=1200M
+#SBATCH --account=def-sponsor00
+#SBATCH --output=/home/user001/projects/def-sponsor00/user001/OUTPUTDIR/HelloWorld.out
+
+echo "Begin script"
+echo "HELLO WORLD"
+echo "End script"
+
